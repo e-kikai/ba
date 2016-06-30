c64ca767596d3a668416206d5147013334f68636
川端洋平 <bata44883@gmail.com>
2016-06-30 19:00:13 +0900

    staging product capistrano

--- db/migrate/20160122072240_client_columns_change_summally.rb
+++ db/migrate/20160122072240_client_columns_change_summally.rb
@@ -1,7 +1,7 @@
 class ClientColumnsChangeSummally < ActiveRecord::Migration
   def change
     change_column :client_columns, :sumally, :text, null: true
-    rename_column :client_columns, :sumally, :comment
+    rename_column :client_columns, :sumally, :comment2
 
     add_column :client_columns, :sumally, :boolean, null: false, default: false
 
