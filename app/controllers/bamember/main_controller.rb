82df1a4c53b9c64494a3e92e8681d444b197f17c
川端洋平 <bata44883@gmail.com>
2016-08-24 15:49:52 +0900

    no message

--- app/controllers/bamember/main_controller.rb
+++ app/controllers/bamember/main_controller.rb
@@ -10,7 +10,7 @@ class Bamember::MainController < Bamember::ApplicationController
     if Rails.env.development?
       Open3.popen3('sleep 2; sudo systemctl restart unicorn_ba;')
     else
-      `nohup sudo /etc/init.d/unicorn_ba restart > /dev/null &`
+      Open3.popen3('sleep 2; nohup sudo /etc/init.d/unicorn_ba restart > /dev/null &')
     end
 
     redirect_to "/bamember/", notice: "Unicorn再起動を実行しました。再起動完了までしばらくお待ち下さい"
